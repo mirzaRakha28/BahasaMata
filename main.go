cdc9a0b1bb8a6f47cd9e0d41f5877fa41f1dc077
mirzaRakha28 <mirzarakhadwipradana28@gmail.com>
2021-04-30 02:14:36 +0700

    aes

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/mirzaRakha28/BahasaMata/db"
 	"github.com/mirzaRakha28/BahasaMata/routes"
 )
 
 func main() {
-	db.Init()
+	// db.Init()
 	e := routes.Init()
 
 	e.Logger.Fatal(e.Start(":3000"))
--- routes/routes.go
+++ routes/routes.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/mirzaRakha28/BahasaMata/controllers"
 )
 
 func Init() *echo.Echo {
@@ -12,18 +11,18 @@ func Init() *echo.Echo {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, this is echo!")
 	})
-	e.POST("/register", controllers.Register)
-	e.POST("/notifikasi", controllers.AddNotifikasi)
-	e.POST("/pasien", controllers.AddPasien)
-	e.POST("/login", controllers.Login)
-	e.POST("/alarm", controllers.AddAlarm)
-	e.POST("/alarm/perawat/:id", controllers.FetchAlarm)
+	// e.POST("/register", controllers.Register)
+	// e.POST("/notifikasi", controllers.AddNotifikasi)
+	// e.POST("/pasien", controllers.AddPasien)
+	// e.POST("/login", controllers.Login)
+	// e.POST("/alarm", controllers.AddAlarm)
+	// e.POST("/alarm/perawat/:id", controllers.FetchAlarm)
 
-	e.PUT("/alarm/:id", controllers.UpdateAlarm)
+	// e.PUT("/alarm/:id", controllers.UpdateAlarm)
 
-	e.DELETE("/pasien", controllers.DeletePasien)
-	e.DELETE("/notifikasi", controllers.DeleteNotifikasi)
-	e.DELETE("/alarm/:id", controllers.DeleteAlarm)
+	// e.DELETE("/pasien", controllers.DeletePasien)
+	// e.DELETE("/notifikasi", controllers.DeleteNotifikasi)
+	// e.DELETE("/alarm/:id", controllers.DeleteAlarm)
 
 	return e
 
